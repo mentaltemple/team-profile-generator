51dc1c5ab0609a9b9682a4d131b6d6aa65ebebef
Joshua Abeyta <mentaltemple@gmail.com>
2021-05-17 22:52:34 -0600

    added prompts to collect data, began proxy html styling

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const questions = require("./lib/questions");
 
-function init() 
+function writeToFile(fileName, data) {
+  return fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
+
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    function addMember() {
+      if (data.teamMember === "Engineer") {
+        console.log("engineer");
+        // inquirer.prompt(Engineer);
+      } else if (data.teamMember === "Intern") {
+        console.log("intern");
+        // inquirer.prompt(Intern);
+      } else
+        console.log(
+          "\nThank you, your team profile has been successfully generated!"
+        );
+      process.exit(0);
+    }
+
+    addMember(data);
+    // writeToFile("index.html", generateTeam(data));
+  }).catch;
+}
+
+init();
+//
